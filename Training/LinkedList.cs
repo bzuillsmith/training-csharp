31c294fed76e44f74b3816906641677cf6cda05e
Lenny Castaneda <lenny@gdsestimating.com>
2018-02-01 15:20:46 -0800

    Completed Find() and Remove() functions for unit tests.

--- Training/LinkedList.cs
+++ Training/LinkedList.cs
@@ -11,12 +11,48 @@ namespace Training
 
         public void Add(int item)
         {
-            throw new NotImplementedException();
+            if (First != null)
+            {
+                var newNode = new LinkedListNode();
+                Last.Next = newNode;
+                Last = newNode;
+                newNode.Value = item;
+            }
+            else
+            {
+                First = new LinkedListNode();
+                First.Value = item;
+                Last = First;
+            }
+            Count += 1;
         }
 
         public LinkedListNode Find(int value)
         {
-            throw new NotImplementedException();
+            // If the LinkedList is empty, return nothing
+            if (First == null) return null;
+
+            /// Create a new pointer
+            LinkedListNode temp = null;
+
+            // Point that pointer to the First node
+            temp = First;
+
+            // While temp is not pointing to the last node
+            while (temp != null)
+            {
+                // If the value inside temp no is the value we're looking for
+                if (temp.Value == value)
+                {
+                    // Return it
+                    return temp;
+                }
+
+                // Reconnect the linked list
+                temp = temp.Next;
+            }
+
+            return null;
         }
 
         public LinkedListNode FindLast(int value)
@@ -31,7 +67,58 @@ namespace Training
 
         public void Remove(int item)
         {
-            throw new NotImplementedException();
+            if (First != null)
+            {
+                //Find the node with the item
+                LinkedListNode previous = null;
+
+                LinkedListNode current = null;
+                current = First;
+
+                while (current != null)
+                {
+                    // Check if current is what we are looking for
+                    if (current.Value == item)
+                    {
+                        // If we are at the first node then just change First to next node.
+                        if (First == current)
+                        {
+                            First = First.Next;
+                            return;
+                        }
+
+                        // Redirect previous to what is after current
+                        previous.Next = current.Next;
+
+                        // if the previous node next property points to nothing, it is the last node now
+                        if(previous.Next == null)
+                        {
+                            // Point Last node to previous node
+                            Last = previous;
+                        }
+
+                        return;
+                    }
+                    // else lets move to the next node
+                    else
+                    {
+                        previous = current;
+                        current = current.Next;
+                    }
+                }
+
+                if (current == null)
+                {
+                    throw new InvalidOperationException("Attempting to remove an item not in the list should throw an exception.");
+                }
+            }
+            else
+            {
+                throw new InvalidOperationException("Attempting to remove an item from an emtpy list should throw an exception.");
+            }
+
+            // Update Count
+            Count -= 1;
         }
     }
 
